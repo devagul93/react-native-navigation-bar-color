da83e8b92f6b5785338804c12a373c96645ce0f3
Devansh Gulhane <devagul93@gmail.com>
2019-07-15 01:58:41 +0530

    added hasNavBar and height

--- src/index.js
+++ src/index.js
@@ -1,25 +1,47 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform } from "react-native";
 
 const { NavigationBarColor } = NativeModules;
 
 const changeNavigationBarColor = (color = String, light = false) => {
-  if(Platform.OS === 'android') {
+  if (Platform.OS === "android") {
     const LightNav = light ? true : false;
     NavigationBarColor.changeNavigationBarColor(color, LightNav);
   }
 };
 const HideNavigationBar = () => {
-  if(Platform.OS === 'android') {
+  if (Platform.OS === "android") {
     return NavigationBarColor.HideNavigationBar();
   } else {
     return false;
   }
 };
 const ShowNavigationBar = () => {
-  if(Platform.OS === 'android') {
+  if (Platform.OS === "android") {
     NavigationBarColor.ShowNavigationBar();
   } else {
     return false;
   }
 };
-export { changeNavigationBarColor, HideNavigationBar, ShowNavigationBar };
+
+const HasNavBar = () => {
+  if (Platform.OS === "android") {
+    NavigationBarColor.HasNavBar();
+  } else {
+    return false;
+  }
+};
+
+const HasNavBarHeight = () => {
+  if (Platform.OS === "android") {
+    NavigationBarColor.HasNavBarHeight();
+  } else {
+    return false;
+  }
+};
+export {
+  changeNavigationBarColor,
+  HideNavigationBar,
+  ShowNavigationBar,
+  HasNavBar,
+  HasNavBarHeight
+};
